49164844e1315ae74af10b74b5c60ec13f5e57b8
EFTEKHER <eftekherali2000@gmail.com>
2024-04-02 00:32:37 +0600

    done

--- react-basic-1/src/Color.jsx
+++ react-basic-1/src/Color.jsx
@@ -15,7 +15,7 @@ const Color = () => {
         <div className=' h-screen w-screen flex' style={{backgroundColor:color}}>
             <div className='fixed   px-4 z-10 bottom-12 inset-x-0 overflow-x-auto flex bg-white m-auto justify-center items-center'>
                 {colors.map((col, i) => (
-                    <button key={i} onClick={() => submitColor(col)} className=' w-20 h-10 rounded-xl m-1 flex p-2 text-white outline-none shadow-lg' style={{backgroundColor:col}}>{col}</button>
+                    <button key={i} onClick={() => submitColor(col)} className=' w-20 h-10 rounded-xl m-1 flex p-2 text-black outline-none shadow-lg' style={{backgroundColor:col}}>{col}</button>
                 ))}
             </div>
         </div>
